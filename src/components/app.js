9e4aed68605ef665f021645a29c85ff7323edf64
Chikara Okada <chikara@gmail.com>
2018-05-04 15:18:36 +0900

    Booking confirmation details added; Need to preserve state when refreshed.

--- src/components/app.js
+++ src/components/app.js
@@ -5,7 +5,7 @@ import Header from './auth/header';
 export default class App extends Component {
   componentWillMount() {
     const API_KEY = process.env.GOOGLEMAP_API_KEY;
-    console.log('in app.js, compoenentWillMount, API_KEY: ', API_KEY)
+    // console.log('in app.js, compoenentWillMount, API_KEY: ', API_KEY)
     const script = document.createElement('script');
     script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
     document.head.append(script);
@@ -20,9 +20,3 @@ export default class App extends Component {
     );
   }
 }
-
-// const App = () => (
-//   <div>
-//     <Main />
-//   </div>
-// );
--- src/components/booking_confirmation.js
+++ src/components/booking_confirmation.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class BookingConfirmation extends Component {
+    renderBookingData() {
+      if (this.props.flat && this.props.bookingData) {
+        return (
+          <div>
+            <h2>
+              Thank you for booking with us!
+            </h2>
+            <h3>
+              You can manage your bookings in My Page.
+            </h3>
+            <div className="booking-confirmation">
+              <div>
+                Description: {this.props.flat.description}
+              </div>
+              <div>
+                Area: {this.props.flat.area}
+              </div>
+              <div>
+                Beds: {this.props.flat.beds}
+              </div>
+              <div>
+                Booking start: {this.props.bookingData.date_start}
+              </div>
+              <div>
+                Booking end: {this.props.bookingData.date_end}
+              </div>
+            </div>
+          </div>
+        );
+      }
+  }
+    // renderBookingData() {
+    //   if (this.props.flat && this.props.bookingData) {
+    //     return (
+    //       <div>
+    //         Booking Data
+    //       </div>
+    //     );
+    //   }
+    // }
   render() {
     return (
       <div>
-        <div>
-          Thank you for booking with us!
-        </div>
-        <div>
-          You can manage your booking in My Page.
-        </div>
+        {this.renderBookingData()}
       </div>
     );
   }
 }
 
-export default BookingConfirmation;
+function mapStateToProps(state) {
+  console.log('in booking confirmation, mapStateToProps, state: ', state);
+  return {
+    bookingData: state.bookingData.bookingData,
+    flat: state.flat.selectedFlat
+  };
+}
+
+export default connect(mapStateToProps)(BookingConfirmation);
