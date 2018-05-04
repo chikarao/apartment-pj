f5e79fbb2a8dfb4baffbf59c801fd52e13ecb1f0
Chikara Okada <chikara@gmail.com>
2018-05-04 20:01:41 +0900

    script.async and .defer added to App.js cwm to address gmap async issues. Seems to be working... needs further testing...

--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,10 @@ export default class App extends Component {
     // console.log('in app.js, compoenentWillMount, API_KEY: ', API_KEY)
     const script = document.createElement('script');
     script.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}`;
+    // added async and defer to make sure gmap loads before component...
+    // https://medium.com/@nikjohn/speed-up-google-maps-and-everything-else-with-async-defer-7b9814efb2b
+    script.async = true;
+    script.defer = true;
     document.head.append(script);
   }
 
