e1678b048419ca1f01784827b7552ef956ebf2d4
Chikara Okada <chikara@gmail.com>
2018-06-22 17:30:51 +0900

    Pagination; added double arrow when in last set of page boxes to take user back to page 1. Now user not stranded after skipping to last set.

--- src/components/pagination/pagination.js
+++ src/components/pagination/pagination.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 import _ from 'lodash';
+// import { connect } from 'react-redux';
+// import { withRouter } from 'react-router-dom';
 
-import * as actions from '../actions';
-import GoogleMap from './maps/google_map';
-import Upload from './images/upload';
-import MainCards from './cards/main_cards';
-import Pagination from './pagination/pagination';
+import * as actions from '../../actions';
 
-// const publicId = ['RPP9419_mp7xjn', 'redbrick_bklymp', 'dewhirst_electric_co_lofts-01_oxgife', 'flat_image-1523948892-1', 'flat_image-1523948892-0'];
-
-const initialPosition = {
-  lat: 37.7952,
-  lng: -122.4029
-};
-
-// lastPage set as global since setState cannot be run in renderfunctions
 let lastPage = 1;
 let displayedPagesArray = [];
 const MAX_NUM_PAGE_NUMBERS = 5;
-// let displayedPagesArray;
 
-class Results extends Component {
+// THIS IS A REUSABLE COMPONENT. JUST PASS FLATS OR WHATEVER ELEMENTS AS Flat
+// PROPS AND THIS WILL PAGINATE
+// CAN BE USED FOR THE RESULTS PAGE IF CURRENT PAGE IS SET WITH APP STATE
+
+class Pagination extends Component {
   constructor() {
   super();
   this.state = {
@@ -47,136 +39,7 @@ class Results extends Component {
 }
 // Pagination reference: https://stackoverflow.com/questions/40232847/how-to-implement-pagination-in-reactjs
 
-  componentDidMount() {
-    // let index = 1;
-    // console.log('in results componentDidMount, index: ', index);
 
-    // Set up initial mapBounds to make Mapbounds not undefined in action fetchFlats
-    // When able to obtain user location or search location, enter initial position here
-    // SF Transamerica touwer
-
-    // initial position offsets; based on zoom twelve of
-    //SF area showing tip of marin counth, tip of the sf peninsula, and tip of oakland
-    // const latOffsetNorth = 0.06629999999999825;
-    // const latOffsetSouth = -0.036700000000003286;
-    // const lngOffsetWest = -0.13469999999999516;
-    // const lngOffsetEast = 0.11589000000000738;
-    // wider offsets; reaches Oakland
-    const latOffsetNorth = 0.06629999999999825;
-    const latOffsetSouth = -0.036700000000003286;
-    const lngOffsetWest = -0.14;
-    const lngOffsetEast = 0.2;
-    // const latOffsetNorth = 37.8615 - initialPosition.lat; //about .07
-    // const latOffsetSouth = 37.7585 - initialPosition.lat; // about -.04
-    // const lngOffsetWest = -122.28701 - initialPosition.lng; //about .12
-    // const lngOffsetEast = -122.5376 - initialPosition.lng; // about -.13
-
-    console.log('in results componentDidMount, Offsets, north, south, east, west: ', latOffsetNorth, latOffsetSouth, lngOffsetEast, lngOffsetWest);
-
-    const mapBounds = {
-      east: (initialPosition.lng + lngOffsetEast),
-      west: (initialPosition.lng + lngOffsetWest),
-      north: (initialPosition.lat + latOffsetNorth),
-      south: (initialPosition.lat + latOffsetSouth)
-    };
-
-    // this.props.startUpIndex();
-    //initial call of fetchFlats to get initial set of flats, RIGHT NOW NOT BASED ON MAP mapBounds
-    // fetchflats based on above bounds
-    this.props.fetchFlats(mapBounds);
-    if (this.props.auth.authenticated) {
-      this.props.fetchLikesByUser();
-    }
-  }
-
-  componentDidUpdate() {
-    // this.setState({ componentUpdated: true });
-  }
-
-  // componentWillReceiveProps() {
-  // }
-  calculateLatLngAve(flats) {
-    let totalLat = 0;
-    let totalLng = 0;
-    let totalNumFlats = 0;
-    _.map(flats, (flat) => {
-      if (flat.lat && flat.lng) {
-        totalLat += flat.lat;
-        totalLng += flat.lng;
-        totalNumFlats++;
-      }
-    });
-    const aveLat = totalLat / totalNumFlats;
-    const aveLng = totalLng / totalNumFlats;
-    return (
-      {
-        lat: aveLat,
-        lng: aveLng
-      }
-    );
-  }
-
-  renderMap() {
-    const flatsEmpty = _.isEmpty(this.props.flats);
-    const mapDimensionsEmpty = _.isEmpty(this.props.mapDimensions);
-    console.log('in results renderMap, flats empty: ', flatsEmpty);
-    console.log('in results renderMap, mapDimensions empty: ', mapDimensionsEmpty);
-
-    if (!flatsEmpty) {
-      if (!mapDimensionsEmpty) {
-        console.log('in results, renderMap this.props.mapDimensions, mapBounds: ', this.props.mapDimensions.mapDimensions.mapBounds);
-        console.log('in results, this.props.mapDimensions, mapCenter.lat: ', this.props.mapDimensions.mapDimensions.mapCenter.lat());
-        console.log('in results, this.props.mapDimensions, mapCenter.lng: ', this.props.mapDimensions.mapDimensions.mapCenter.lng());
-        console.log('in results, this.props.mapDimensions, mapZoom: ', this.props.mapDimensions.mapDimensions.mapZoom);
-      }
-      // const { id } = this.props.flats[0];
-      // console.log('in results renderFlats, id: ', id);
-      console.log('here is the average lat lng, results from calculateLatLngAve: ', this.calculateLatLngAve(this.props.flats));
-      const latLngAve = this.calculateLatLngAve(this.props.flats);
-      console.log('here is latLngAve: ', latLngAve);
-
-      return (
-        <div>
-        <GoogleMap
-        // key={'1'}
-        flatsEmpty={flatsEmpty}
-        flats={this.props.flats}
-        initialPosition={latLngAve || initialPosition}
-        currency='$'
-        />
-        </div>
-      );
-      // <div>{console.log('in div: ', flats)}</div>
-    } else if (!mapDimensionsEmpty) {
-      // return <div>Map Goes Here</div>;
-      const emptyMapLatLngCenter = {
-        lat: this.props.mapDimensions.mapDimensions.mapCenter.lat(),
-        lng: this.props.mapDimensions.mapDimensions.mapCenter.lng()
-      };
-      return (
-        <div>
-        <GoogleMap
-        flatsEmpty={flatsEmpty}
-        flats={flatsEmpty ? this.props.flats : emptyMapLatLngCenter}
-        // initialPosition={emptyMapLatLngCenter}
-        initialZoom={this.props.mapDimensions.mapDimensions.mapZoom}
-        />
-        </div>
-      );
-    } else {
-      return (
-        <div>
-        <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-        <div className="spinner">Loading...</div>
-        </div>
-      );
-    } // if not empty
-  }
-
-  //*******************************************************************************
-  //*********************PAGINATION****************************************
-  // THIS IS THE SAME CODE AS SRC/COMPONENTS/PAGINATION/PAGINATION AS OF 6/22/2018
-  // PAGINATION.JS CAN BE USED IF CURRENT PAGE IS SET IN APP STATE
   removeCurrent() {
     const currentPageLi = document.getElementsByClassName('current');
     if (currentPageLi[0]) {
@@ -457,8 +320,6 @@ class Results extends Component {
       }
   }
 
-
-
   handleDoubleLeftPageClick() {
     this.removeCurrent();
     this.setState({
@@ -528,110 +389,25 @@ class Results extends Component {
       }
     }
   }
-  //*******************************************************************************
-  //*********************PAGINATION****************************************
-
-  renderFlats() {
-    let index = 1;
-    console.log('in results renderFlats, flats data length: ', this.props.flats);
-    const flatsEmpty = _.isEmpty(this.props.flats);
-    const mapDimensionsEmpty = _.isEmpty(this.props.mapDimensions);
-    console.log('in results renderFlats, flats empty: ', flatsEmpty);
-    console.log('in results renderFlats, this.props.startUpCount.startUpCount: ', this.props.startUpCount.startUpCount);
-    // const randomNum = _.random(0, 1);
-    // console.log('in results renderFlats, randomNum: ', randomNum);
-
-      // if (!flatsEmpty && randomNum === 1) {
-      if (!flatsEmpty) {
-        // console.log('in results renderFlats, this.props.flats.rooms: ', this.props.flats.rooms);
-        // const { id } = this.props.flats[0];
-        // console.log('in results renderFlats, id: ', id);
-        const { currentPage, cardsPerPage } = this.state;
-        // console.log('in results renderPagination, flatsEmpty: ', flatsEmpty);
-        // console.log('in results renderPagination, currentPage: ', currentPage);
-        // console.log('in results renderPagination, cardsPerPage: ', cardsPerPage);
-        const cards = this.props.flats;
-        console.log('in results renderPagination, cards: ', cards);
-        // Logic for displaying todos
-        const indexOfLastCard = currentPage * cardsPerPage;
-        const indexOfFirstCard = indexOfLastCard - cardsPerPage;
-        //slice reference: https://stackoverflow.com/questions/39336556/how-can-i-slice-an-object-in-javascript
-        const slicedCards = Object.entries(cards).slice(indexOfFirstCard, indexOfLastCard).map(entry => entry[1]);
-        // console.log('in results renderFlats, slicedCards, currentPage: ', currentPage);
-        // console.log('in results renderFlats, slicedCards, indexOfFirstCard: ', indexOfFirstCard);
-        // console.log('in results renderFlats, slicedCards, indexOfLastCard: ', indexOfLastCard);
-        // console.log('in results renderFlats, slicedCards: ', slicedCards);
-        const mappedSlicedCards = _.mapKeys(slicedCards, 'id');
-
-        const flats = mappedSlicedCards;
-        // const flats = this.props.flats;
-        console.log('in results renderFlats, slicedCards, slicedCards: ', slicedCards);
-        console.log('in results renderFlats, slicedCards, mappedSlicedCards: ', mappedSlicedCards);
-        console.log('in results renderFlats, slicedCards, this.props.flats: ', this.props.flats);
-
-        return _.map(flats, (flat) => {
-            return (
-
-              <div key={flat.id.toString()}>
-                <MainCards
-                  // key={flat.id.toString()}
-                  flat={flat}
-                  likes={this.props.likes}
-                  currency='$'
-                  showFlat={false}
-                  authenticated={this.props.auth.authenticated}
-                />
-              </div>
-            );
-          });
-          // <div>{console.log('in results renderFlats, flats: ', flats)}</div>
-          // this.props.startUpIndex();
-      } else if (this.props.startUpCount.startUpCount !== 0) {
-        return <div className="no-results-message">No flats in that area or with that criteria. <br/>Please search again!</div>;
-      } else {
-        return (
-          <div>
-            <i className="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-            <div className="spinner">Loading flats...</div>
-          </div>
-        );
-      }
-  }
-
 
   render() {
-    // <Pagination
-    // flats={this.props.flats}
-    // />
     return (
-      <div>
-        <div className="container" id="map">
-          {this.renderMap()}
-        </div>
-
-        <div className="container main-card-container">
-          <div className="row card-row">
-            {this.renderFlats()}
-          </div>
-        </div>
-        <div className="pagination-container">
-          {this.renderPagination()}
-        </div>
-      </div>
+      <div>{this.renderPagination()}</div>
     );
   }
 }
+// messaging is designed to be used on my page and show flat
+// takes booleans fromShowPage, currentUserIsOwner passed from show flat page and
+// Note that uses conversation (singular) passed as props from show flat and covnversations (plural) from app state
+// uses app state so that the component rerenders when a message is sent
+// function mapStateToProps(state) {
+//   console.log('in messaging, mapStateToProps, state: ', state);
+//   return {
+//     auth: state.auth,
+//     // conversations: state.conversation.conversationByUserAndFlat,
+//     // noConversation: state.conversation.noConversation,
+//     // flat: state.flat.selectedFlatFromParams
+//   };
+// }
 
-function mapStateToProps(state) {
-  console.log('in results mapStateToProps: ', state);
-  return {
-    message: state.auth.message,
-    flats: state.flats,
-    startUpCount: state.startUpCount,
-    mapDimensions: state.mapDimensions,
-    likes: state.likes.userLikes,
-    auth: state.auth
-   };
-}
-
-export default connect(mapStateToProps, actions)(Results);
+export default Pagination;
