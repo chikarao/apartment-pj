0211787e7dabd48ca6add5c9861f77bb84ed65b3
Chikara Okada <chikara@gmail.com>
2018-07-06 12:55:19 +0900

    Banner search drop down from scratch; Still buggy but most of it done. When click in input and start typing, if there is some match, drop down opens with display: none changed to block based on this.state.displayCitiesList. Arrow down highlights li and removes hightlight with background color. etc.

---
+++ src/components/constants/cities_list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const CitiesList =
+  [
+    { name: 'San Francisco', lat: 0, lng: 0 },
+    { name: 'Seattle', lat: 0, lng: 0 },
+    { name: 'New York', lat: 0, lng: 0 },
+    { name: 'Los Angeles', lat: 0, lng: 0 },
+    { name: 'Chicago', lat: 0, lng: 0 },
+    { name: 'Austin', lat: 0, lng: 0 },
+    { name: 'Portland', lat: 0, lng: 0 },
+    { name: 'Pittsburgh', lat: 0, lng: 0 }
+  ];
+
+export default CitiesList;
--- src/components/landing.js
+++ src/components/landing.js
@@ -3,10 +3,12 @@ import { connect } from 'react-redux';
 import { Image, Transformation, CloudinaryContext } from 'cloudinary-react';
 import cloudinary from 'cloudinary-core';
 
+
 // import Upload from './images/upload_test';
 // import SigninModal from './auth/signin_modal';
 // import SignupModal from './auth/signup_modal';
 import * as actions from '../actions';
+import citiesList from './constants/cities_list'
 
 const CLOUD_NAME = process.env.CLOUDINARY_CLOUD_NAME;
 const cloudinaryCore = new cloudinary.Cloudinary({ cloud_name: CLOUD_NAME });
@@ -15,7 +17,12 @@ class Landing extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      windowWidth: window.innerWidth
+      windowWidth: window.innerWidth,
+      // searchInput: ''
+      searchInputHasValue: false,
+      displayCitiesList: false,
+      citiesSubsetArray: [],
+      selectedCity: ''
     };
   }
 
@@ -40,25 +47,151 @@ class Landing extends Component {
     return cloudinaryCore.url(image, t);
   }
 
+  handleBannerSearchClick() {
+    // console.log('in landing, handleBannerSearchClick, event.target: ', event.target);
+    // const clickedElement = event.target;
+    // const elementVal = clickedElement.getAttribute('value')
+    // console.log('in landing, handleBannerSearchClick, elementVal: ', elementVal);
+    console.log('in landing, handleSearchInputChange, this.state.selectedCity: ', this.state.selectedCity);
+    // this.props.history.push('/results');
+  }
+
+  // renderDataListOptions() {
+  //   return (
+  //     <div>
+  //       <option value="San Francisco" />
+  //       <option value="Seattle" />
+  //       <option value="New York" />
+  //       <option value="Los Angeles" />
+  //       <option value="Chicago" />
+  //       <option value="Austin" />
+  //       <option value="Portland" />
+  //       <option value="Pittsburgh" />
+  //     </div>
+  //   );
+  // }
+  renderCitiesList() {
+    const cities = this.state.citiesSubsetArray;
+    return _.map(cities, city => {
+      return (
+        <li key={city.name} value={city.name}>{city.name}</li>
+      );
+    });
+  }
+
+  getCitiesSubsetArray(searchInput) {
+    const cities = citiesList;
+    const citiesArray = [];
+    const searchInputLower = searchInput.toLowerCase();
+    _.each(cities, city => {
+      const cityName = city.name.toLowerCase();
+      if (cityName.includes(searchInputLower)) {
+        citiesArray.push(city)
+      }
+    });
+    return citiesArray;
+  }
+
+  handleSearchInputChange(searchInput) {
+    const citiesSubsetArray = this.getCitiesSubsetArray(searchInput);
+    console.log('in landing, handleSearchInputChange, citiesSubsetArray: ', citiesSubsetArray);
+    if (searchInput !== '') {
+      this.setState({ searchInputHasValue: true, searchInput, citiesSubsetArray, displayCitiesList: true }, () => {
+        // console.log('in landing, handleSearchInputChange, this.state.searchInputHasValue: ', this.state.searchInputHasValue);
+        // console.log('in landing, handleSearchInputChange, this.state.searchInput: ', this.state.searchInput);
+
+      });
+      this.scrollList();
+    } else {
+      this.setState({ searchInputHasValue: false, citiesSubsetArray, displayCitiesList: false }, () => {
+        // console.log('in landing, handleSearchInputChange, this.state.searchInputHasValue: ', this.state.searchInputHasValue);
+      });
+    }
+  }
+
+  scrollList() {
+    const list = document.getElementById('banner-search-cities-list');
+    const first = list.firstChild;
+    let active = first;
+    console.log('in landing, scrollList, first, list: ', first, list);
+    const mainInput = document.getElementById('banner-input');
+    console.log('in landing, scrollList, mainInput: ', mainInput);
+
+    document.onkeydown = (e) => { // listen to keyboard events
+       switch (e.keyCode) {
+         case 38: // if the UP key is pressed
+         // console.log('in landing, scrollList, UP clicked, document.activeElement: ', document.activeElement);
+         // console.log('in landing, scrollList, UP clicked, document.activeElement.nextSibling: ', document.activeElement.nextSibling);
+         console.log('in landing, scrollList, UP clicked, active.getAttribute, first.getAttribute: ', active.getAttribute('value'), first.getAttribute('value'));
+           if (document.activeElement == mainInput || (active.getAttribute('value') == first.getAttribute('value'))) {
+             active.style.backgroundColor = 'white ';
+             mainInput.focus();
+             break;
+           } else {
+             active.style.backgroundColor = 'white ';
+             active = active.previousSibling;
+             active.style.backgroundColor = 'lightgray';
+             const selectedCity = active.getAttribute('value');
+             this.setState({ selectedCity }, () => {
+              mainInput.value = this.state.selectedCity;
+             })
+           } // select the element before the current, and focus it
+           break;
+         case 40: // if the DOWN key is pressed
+         // console.log('in landing, scrollList, DOWN clicked, document.activeElement, first.firstChild: ', document.activeElement, list.firstChild);
+         // console.log('in landing, scrollList, DOWN clicked, document.activeElement.nextSibling: ', document.activeElement.nextSibling);
+           if (document.activeElement == mainInput) {
+             active = list.firstChild;
+             list.firstChild.style.backgroundColor = 'lightgray';
+             document.activeElement.blur();
+           } else {
+             // document.activeElement.nextSibling.firstChild.setActive();
+             if(active.nextSibling) {
+               active.style.backgroundColor = 'white ';
+               active = active.nextSibling;
+               active.style.backgroundColor = 'lightgray';
+               const selectedCity = active.getAttribute('value');
+               this.setState({ selectedCity }, () => {
+                 mainInput.value = this.state.selectedCity;
+                 console.log('in landing, scrollList, DOWN clicked, this.state.selectedCity: ', this.state.selectedCity);
+               })
+             } else {
+               break;
+             }
+           } // target the currently focused element -> <a>, go up a node -> <li>, select the next node, go down a node and focus it
+         break;
+
+         case 13:
+         if (document.activeElement == mainInput || (active.getAttribute('value') == first.getAttribute('value'))) {
+           console.log('in landing, scrollList, ENTER clicked, mainInput or first: ');
+         } else {
+           this.setState({ displayCitiesList: false })
+         }
+         break;
+       }
+     }
+  }
+
   renderBanner() {
     // <h1>Flats, flats & more flats</h1>
     console.log('in landing, renderBanner, this.state.windowWidth: ', this.state.windowWidth);
-    // <option value="Seattle" />
-    // <option value="New York" />
-    // <option value="Los Angeles" />
-    // <option value="Chicago" />
-    // <option value="Austin" />
-    // <option value="Portland" />
-    // <option value="Pittsburgh" />
+    // <div className="banner-search-datalist-div">
+    // </div>
+    // <datalist className="banner-datalist" id="areas">
+    // {this.state.searchInputHasValue ? this.renderDataListOptions() : ''}
+    // </ datalist>
     return (
       <div className="banner" style={{ background: `url(${this.createBackgroundImage('banner_image_1')}` }}>
         <div className="banner-content">
           <div className="banner-search-box">
-            <label><input list="areas" className="banner-search-input" type="string" placeholder="Search for flats in a city..." /></label>
-              <datalist id="areas">
-                <option value="San Francisco" />
-              </ datalist>
-            <button className="banner-search-button">Search</button>
+            <div className="banner-search-input-and-button">
+              <label><input list="areas" value={this.state.selctedCity} className="banner-search-input" id="banner-input" type="string" onChange={event => this.handleSearchInputChange(event.target.value)} placeholder="Find flats in a city!" /></label>
+              <button className="banner-search-button" onClick={this.handleBannerSearchClick.bind(this)}>Search</button>
+            </div>
+            <ul className="cities-list-ul" id="banner-search-cities-list"style={this.state.displayCitiesList ? { display: 'block' } : { display: 'none' }} >
+              <li key={1} value="">--</li>
+              {this.renderCitiesList()}
+            </ul>
           </div>
           <p>Freedom and simplicity in where you live</p>
         </div>
