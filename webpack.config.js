b10d659ca6934da76dbdcb18cd62def94861337e
Chikara Okada <chikara@gmail.com>
2018-09-08 08:59:12 +0900

    Updated webpack 1 to 4 (changed webpack config to update to wp4. Many changesKept babel-core 6 since cannot 7 cannot work with babel-loader 8 for some reason. Webpack-dev-server to v3.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 
+const path = require('path');
+
 // reference for babel and polyfill; async await
 // https://stackoverflow.com/questions/33527653/babel-6-regeneratorruntime-is-not-defined
 // Reference: migration from webpack 1 to webpack 4 https://webpack-gatsby.netlify.com/how-to/upgrade-from-webpack-1/
 
 module.exports = {
   // polyfill for async await
+  mode: 'development',
   entry: [
     'babel-polyfill', './src/index.js'
   ],
@@ -16,28 +19,44 @@ module.exports = {
     filename: 'bundle.js'
   },
   module: {
-    loaders: [{
-      exclude: /node_modules/,
-      loader: 'babel',
-      query: {
-        // presets: ['react', 'es2015', 'stage-1']
-        // state-0 for async await
-        presets: ['react', 'es2015', 'stage-0']
+    // file-loader doc: https://github.com/webpack-contrib/file-loader
+    // wp4 loaders changed to rules
+    rules: [
+      {
+        test: /\.(jpg|png|svg)$/,
+        // wp4 loaders changed to use
+        use: [
+          {
+            loader: 'file-loader',
+            // query changed to options in wp4
+            options: {
+            // regExp: /\/([a-z0-9]+)\/[a-z0-9]+\.png$/,
+              name: '[path][name].[hash].[ext]'
+            // context: ''
+            }
+          }
+        ]
+      },
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use:
+          { loader: 'babel-loader' }
+       //  options: {
+       //   presets: ['react', 'es2015', 'stage-0'],
+       //   plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+       // }
       }
-    },
+      // options: {
+      //   // presets: ['react', 'es2015', 'stage-1']
+      //   // state-0 for async await
+      //   presets: ['react', 'es2015', 'stage-0']
+
     // **********for importing png https://blog.hellojs.org/importing-images-in-react-c76f0dfcb552
     //https://stackoverflow.com/questions/35568114/cannot-load-png-files-with-webpack-unexpected-character/35568830#35568830
     // use either url loader or file-loader; file loader good for production so use
-    {
-      test: /\.(jpg|png|svg)$/,
-      loader: 'file-loader',
-      options: {
-        // regExp: /\/([a-z0-9]+)\/[a-z0-9]+\.png$/,
-        name: '[path][name].[hash].[ext]',
-      },
-    }
     // **********
-    ] // end of loaders
+    ] // end of loaders !!! now rules
     // for react-day-picker !!!!!!!!!!! Decided to use just a link tag in index.html
     //to import css from unpkg
     // rules: [
@@ -50,8 +69,19 @@ module.exports = {
       // for react-day-picker
   }, // end of module
   // added resolve .css and root: __dirname for react-day-picker
+  // resolve new in wp4
   resolve: {
-    extensions: ['', '.js', '.jsx']
+    extensions: ['*', '.js', '.jsx'],
+    modules: [
+     path.join(__dirname, 'src'),
+     'node_modules'
+   ],
+   // test: /\.(jpg|png|svg)$/,
+   // loader: 'file-loader',
+   // options: {
+   //   // regExp: /\/([a-z0-9]+)\/[a-z0-9]+\.png$/,
+   //   name: '[path][name].[hash].[ext]',
+   // },
     // root: __dirname
   },
   devServer: {
